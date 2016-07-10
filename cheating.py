ed1b8c50b90f1baf2724519927d8d437db4efa9d
Aishwarya Sreenivasan (aishsree) <aishsree@cisco.com>
2016-07-09 17:30:02 -0700

    first commit

---
+++ cheating.py
@@ -0,0 +1,14 @@
+class save(object):
+    def __init__(self,action,target):
+        self.action=action
+        self.target=target
+
+    def savings()
+        if(self.action=="buy")
+            if(self.target=="car")
+            if(self.target=="xbox")
+            if(self.target=="cellphone")
+        if(self.action=="study")
+        if(self.action=="spend")
+            if(self.target=="drink")
+            if(self.target=="fruits")
